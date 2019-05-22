55d0d8842c93c610cada9f8a4f2e61bb0a18f239
Asami Doi <doiasami1219@gmail.com>
2019-05-22 11:19:18 +0900

    Add test script.

--- Makefile
+++ Makefile
@@ -3,5 +3,8 @@ files=mdtohtml.go tokenizer.go parser.go generator.go css.go
 mdtohtml: $(files) 
 	go build $(files) 
 
+test: mdtohtml
+	./test.sh
+
 clean:
 	rm mdtohtml
---
+++ test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+try() {
+  expected="$1"
+  input="$2"
+  echo $input > test.md
+
+  ./mdtohtml test.md -nocss
+  actual="$(cat test.html)"
+
+  if [ "$actual" = "$expected" ]; then
+    echo "$input => $actual"
+  else
+    echo "$expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+echo "========== Basic Grammer =========="
+try "<h1>h1</h1>" "# h1"
+try "<h2>h2</h2>" "## h2"
+try "<h3>h3</h3>" "### h3"
+try "<p>dummyh3</p>" "###dummyh3"
+
+rm test.md
+rm test.html
+
+echo OK
