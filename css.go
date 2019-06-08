c5ed27d01725250c6b35c3d215b71e86232298ab
Asami Doi <doiasami1219@gmail.com>
2019-06-08 20:21:50 +0900

    Update font.

--- css.go
+++ css.go
@@ -2,18 +2,20 @@ package main
 
 func css() string {
 	return `
-<link href="https://fonts.googleapis.com/css?family=Abril+Fatface|Lora|Noto+Serif|Noto+Serif+JP|Trirong:700" rel="stylesheet">
 <style>
 body {
-  font-family: 'Noto Sans', sans-serif;
   font-size: 18px;
-  max-width: 740px;
+  max-width: 680px;
   margin: 30px auto 0 auto;
 }
 h1, h2, h3, h4, h5, h6 {
   margin-bottom: 0.5em;
 }
-h1, h2 {
+h1 {
+  font-size: 48px;
+  text-align: center;
+}
+h2 {
   border-bottom: 3px black solid;
 }
 h1 > a, h2 > a {
